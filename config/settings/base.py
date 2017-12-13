c133c52039fa2f0ed5f5d4b32ba5e38708c9d02a
lukeblackman <_@lukeblackman.com>
2017-12-13 22:26:13 +1100

    Static files

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,6 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 import environ
+import os
 
 ROOT_DIR = environ.Path(__file__) - 3  # (body/config/settings/base.py - 3 = body/)
 APPS_DIR = ROOT_DIR.path('body')
